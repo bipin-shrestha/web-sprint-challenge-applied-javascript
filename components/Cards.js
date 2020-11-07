cc0747d9710ea1fa180c938aaf553566256435c2
bipin-shrestha <iambpn.shrestha@gmail.com>
2020-11-06 20:54:15 -0800

    cards Complete

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,44 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+
+axios.get('https://lambda-times-api.herokuapp.com/articles')
+    .then(response => {
+        const articles = response.data.articles;
+        console.log(articles);
+        for (const article in articles) {
+            const item = articles[article];
+            item.forEach(element => {
+                const newCard = articleMaker(element);
+                const cardContainer = document.querySelector('div.cards-container');
+                cardContainer.appendChild(newCard);
+            });
+        }
+    })
+
+function articleMaker(article) {
+    const newDiv = document.createElement('div');
+    newDiv.className = 'card';
+    newDiv.addEventListener('click', () => {
+        console.log(article.headline);
+    })
+    const newHeadline = document.createElement('div');
+    newHeadline.classname = 'headline';
+    newHeadline.textContent = article.headline;
+    newDiv.appendChild(newHeadline);
+    const newAuthor = document.createElement('div');
+    newAuthor.className = 'author';
+    newDiv.appendChild(newAuthor);
+    const imgContainer = document.createElement('div');
+    imgContainer.className = 'img-container';
+    newAuthor.appendChild(imgContainer);
+    const image = document.createElement('img');
+    image.src = article.authorPhoto;
+    imgContainer.appendChild(image);
+    const newSpan = document.createElement('span');
+    newSpan.textContent = 'By ' + article.authorName;
+    newAuthor.appendChild(newSpan);
+
+    return newDiv;
+}
